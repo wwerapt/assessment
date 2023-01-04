37147b79eefbd275748f92c0522124c97bc1f9e9
Weerapat Moungsouy <weerapat.mou@gmail.com>
2023-01-04 16:07:21 +0700

    add expense

---
+++ expense/expense.go
@@ -0,0 +1,9 @@
+package expense
+
+type Expense struct {
+	ID     int      `json:"id"`
+	Title  string   `json:"title"`
+	Amount float64  `json:"amount"`
+	Note   string   `json:"note"`
+	Tags   []string `json:"tags"`
+}
